94a4100888a2ac71f458d8f0156d45acad86ccb0
Ulxn <69082498+enganese@users.noreply.github.com>
2022-01-25 21:12:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-DB_URL = f"postgresql+psycopg2://postgres:ulan290106@127.0.0.1:5432/postgres"
+DB_URL = f"postgres://gcbmnxpbxrnbun:c209a1df2ca1027d7a7773eab6caee4c203e46cda40007801b1e06dc8336ea8f@ec2-52-208-221-89.eu-west-1.compute.amazonaws.com:5432/d53p2nibrvsrbf"
