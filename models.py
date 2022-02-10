726e045355d06073a3132a4f7b976b631b0346d9
Ulxn <69082498+enganese@users.noreply.github.com>
2022-02-10 17:41:04 +0500

    Update models.py

--- models.py
+++ models.py
@@ -26,3 +26,24 @@ class Food(BaseModel):
     description = sa.Column(sa.String(), nullable=True, comment="Описание еды")
     image = sa.Column(sa.String(), default="https://image.freepik.com/free-vector/flat-404-error-template_23-2147745731.jpg", nullable=False, comment="Картинка еды")
     price = sa.Column(sa.BigInteger(), nullable=False, comment="Цена еды")
+
+    
+# class User(BaseModel):
+#     __tablename__ = "users"
+    
+#     id = sa.Column(
+#         sa.BigInteger(), primary_key=True, autoincrement=True, comment="Идентификатор пользователя"
+#     )
+#     name = sa.Column(sa.String(), nullable=False, comment="Имя пользователя")
+#     pfp = sa.Column(sa.String(), default="", nullable=True, comment="Аватар пользователя")
+#     gender = sa.Column(sa.String(), nullable=False, comment="Пол пользователя")
+#     email = sa.Column(sa.String(), nullable=False, comment="Эл. почта пользователя")
+#     username = sa.Column(sa.String(), nullable=False, comment="Юзернейм пользователя")
+#     password = sa.Column(sa.String(), nullable=False, comment="Пароль пользователя")
+#     cart = 
+# #     favorites = relationship("User")
+    
+
+# class Cart(BaseModel):
+#     user_id = sa.Column(sa.ForeignKey("users.id"), primary_key=False, nullable=False, comment="Идентификатор пользователя")
+#     food_id = sa.Column(sa.ForeignKey("foods.id", ondelete="CASCADE"), primary_key=False, nullable=False, comment="Идентификатор еды")
